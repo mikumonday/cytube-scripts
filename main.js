75cebee18debe66605b5fdef69eed54077dff418
Thomas Smith <popadopz@gmail.com>
2014-02-17 22:23:15 -0700

    Add a new area below the playlist
    This area is for permanent information ( mainly links )

--- main.js
+++ main.js
@@ -50,3 +50,20 @@ $('#pictureShow').mouseleave(function () {
 $('#pictureShow').click(function () {
   $('#picture').attr('src', pictures[randPic()].link);
 });
+
+//add a bottom info field
+$('#resizewrap').append(
+"<div id='bottom-info' class='col-lg-12 col-md-12'><div class='well'>" + 
+"<span class='links_main'> - <a href='https://kiwiirc.com/client/irc." + 
+"rizon.net/#mmonday' target='_blank'>irc(#mmonday)</a> ★ <a href='ht" + 
+"tp://mikumonday.com' target='_blank'>mikumonday.com</a> ★ <a href='" + 
+"http://synchtu.be/r/mikumonday' target='_blank'>backup room</a> - </" + 
+"span></br><span class='large'> - <a href='https://www.dropbox" + 
+".com/sh/v8993jax7eksiz1/z28DaYgicB' target='_blank'>Adams lists</a> " + 
+"- <a href='https://plus.google.com/communities/107630350215753976745" + 
+"' target='_blank'>Google+ Community</a> - <a href='https://twitter.c" + 
+"om/MikuMonday' target='_blank'>@MikuMonday</a> - </span></br><span c" + 
+"lass='large'>You can find our channel scripts on <a href='https://gi" + 
+"thub.com/mikumonday/cytube-scripts' target='_blank'>github</a></span></div></div>"
+//lel huge string xdxdxdxdxd
+);
